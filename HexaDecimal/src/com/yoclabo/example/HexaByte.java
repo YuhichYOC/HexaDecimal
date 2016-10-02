39422270d9f77830ffdbba89bb0030f2f83d1e3b
y <y@DESKTOP-20VAU6S>
2016-10-02 10:53:46 +0900

    Work in progress

--- HexaDecimal/src/com/yoclabo/example/HexaByte.java
+++ HexaDecimal/src/com/yoclabo/example/HexaByte.java
@@ -2,62 +2,104 @@ package com.yoclabo.example;
 
 public class HexaByte {
 
-    private byte myValue;
+    private Uint8 myValue;
 
-    public void SetValue(byte value) {
+    public void SetValue(Uint8 value) {
         myValue = value;
-        CastValueToRawStr();
+        FillHexaValue();
     }
 
-    public byte GetValue() {
+    public void SetValue(byte arg) {
+        myValue = Uint8.Value(arg);
+        FillHexaValue();
+    }
+
+    public void SetValue(short arg) {
+        myValue = Uint8.Value(arg);
+        FillHexaValue();
+    }
+
+    public void SetValue(int arg) {
+        myValue = Uint8.Value(arg);
+        FillHexaValue();
+    }
+
+    public Uint8 GetValue() {
         return myValue;
     }
 
-    private String hexaValue;
+    private static char replaceArray[] = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E',
+            'F' };
 
-    public void SetHexa(String value) {
-        myValue = (byte) Integer.parseInt(value, 16);
-        CastValueToRawStr();
+    byte FindPositionInReplaceArray(char arg) {
+        byte retVal = 0;
+        for (byte i = 0; i < 16; i++) {
+            if (replaceArray[i] == arg) {
+                retVal = i;
+            }
+        }
+        return retVal;
+    }
+
+    private byte hexaValue[];
+
+    public void SetHexa(String value) throws IllegalArgumentException {
+        if (value.length() < 2 || value.length() > 2) {
+            throw new IllegalArgumentException("HexaByte hexavalue with length() == 2 String.");
+        }
+        hexaValue = new byte[2];
+        hexaValue[0] = FindPositionInReplaceArray(value.charAt(0));
+        hexaValue[1] = FindPositionInReplaceArray(value.charAt(1));
+        myValue = Uint8.Value(hexaValue[0] * 16 + hexaValue[1]);
     }
 
     public String GetHexa() {
-        return hexaValue;
+        return String.valueOf(replaceArray[hexaValue[0]]) + String.valueOf(replaceArray[hexaValue[1]]);
+    }
+
+    private void FillHexaValue() {
+        hexaValue = new byte[2];
+        hexaValue[0] = (byte) (myValue.GetValue() / 16);
+        hexaValue[1] = (byte) (myValue.GetValue() % 16);
     }
 
     public HexaByte(byte value) {
-        myValue = value;
-        CastValueToRawStr();
+        myValue = Uint8.Value(value);
+        FillHexaValue();
     }
 
-    public HexaByte(String value) {
-        myValue = (byte) Integer.parseInt(value, 16);
-        CastValueToRawStr();
+    public HexaByte(short svalue) {
+        myValue = Uint8.Value(svalue);
+        FillHexaValue();
     }
 
     public HexaByte(int ivalue) {
-        byte value = (byte) ivalue;
-        myValue = value;
-        CastValueToRawStr();
+        myValue = Uint8.Value(ivalue);
+        FillHexaValue();
+    }
+
+    public HexaByte(String value) {
+        SetHexa(value);
     }
 
     public static HexaByte Value(byte value) {
         return new HexaByte(value);
     }
 
-    public static HexaByte Value(String value) {
-        return new HexaByte(value);
+    public static HexaByte Value(short svalue) {
+        return new HexaByte(svalue);
     }
 
     public static HexaByte Value(int ivalue) {
         return new HexaByte(ivalue);
     }
 
+    public static HexaByte Value(String value) {
+        return new HexaByte(value);
+    }
+
     public int toInt() {
-        if (myValue >= 0) {
-            return (int) myValue;
-        } else {
-            return (int) (myValue & 0xff);
-        }
+        return myValue.GetValue();
     }
 
     @Override
@@ -69,17 +111,10 @@ public class HexaByte {
             return false;
         }
         HexaByte otherValue = (HexaByte) other;
-        if (myValue == otherValue.GetValue()) {
+        if (myValue.equals(otherValue.GetValue())) {
             return true;
         } else {
             return false;
-        }
-    }
-
-    private void CastValueToRawStr() {
-        hexaValue = Integer.toHexString(myValue & 0xff);
-        if (hexaValue.length() <= 1) {
-            hexaValue = "0" + hexaValue;
         }
     }
 
---
+++ HexaDecimal/src/com/yoclabo/example/Uint8.java
@@ -0,0 +1,87 @@
+package com.yoclabo.example;
+
+public class Uint8 {
+
+    private byte myValue;
+
+    private byte toByte(short arg) {
+        return (byte) (arg - 128);
+    }
+
+    private byte toByte(int arg) {
+        return (byte) (arg - 128);
+    }
+
+    public void SetValue(byte arg) {
+        myValue = arg;
+    }
+
+    public void SetValue(short arg) throws IllegalArgumentException {
+        if (0 > arg || arg > 256) {
+            throw new IllegalArgumentException("Uint8 accepts value between 0 to 255.");
+        }
+        myValue = toByte(arg);
+    }
+
+    public void SetValue(int arg) throws IllegalArgumentException {
+        if (0 > arg || arg > 256) {
+            throw new IllegalArgumentException("Uint8 accepts value between 0 to 255.");
+        }
+        myValue = toByte(arg);
+    }
+
+    private int toInt() {
+        return myValue + 128;
+    }
+
+    public int GetValue() {
+        return toInt();
+    }
+
+    public Uint8(byte arg) {
+        myValue = arg;
+    }
+
+    public Uint8(short arg) {
+        if (0 > arg || arg > 256) {
+            throw new IllegalArgumentException("Uint8 accepts value between 0 to 255.");
+        }
+        myValue = toByte(arg);
+    }
+
+    public Uint8(int arg) {
+        if (0 > arg || arg > 256) {
+            throw new IllegalArgumentException("Uint8 accepts value between 0 to 255.");
+        }
+        myValue = toByte(arg);
+    }
+
+    public static Uint8 Value(byte arg) {
+        return new Uint8(arg);
+    }
+
+    public static Uint8 Value(short arg) {
+        return new Uint8(arg);
+    }
+
+    public static Uint8 Value(int arg) {
+        return new Uint8(arg);
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        if (this == other) {
+            return true;
+        }
+        if (!(other instanceof Uint8)) {
+            return false;
+        }
+        Uint8 otherValue = (Uint8) other;
+        if (GetValue() == otherValue.GetValue()) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
